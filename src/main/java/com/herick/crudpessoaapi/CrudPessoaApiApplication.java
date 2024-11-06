27e304fd46408c5351f68286dca8aa1c7918dede
Herick2D <contato.herickmoreira@gmail.com>
2024-11-06 16:25:16 -0300

    Init: starting crudPessoaApi

---
+++ src/main/java/com/herick/crudpessoaapi/CrudPessoaApiApplication.java
@@ -0,0 +1,13 @@
+package com.herick.crudpessoaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPessoaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herick/crudpessoaapi/CrudPessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herick.crudpessoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPessoaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
